1fbaf4e3f783c43cab43b14e2ce3c3d4883723b7
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-02 23:14:20 +0000

    How react works

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
 // Notes
 
-// Definition of both React and Redux
 /*
 
-What is REACT?
+How REACT Works!
 
-- Javascript library created by Facebook
-- Also used by Netflix and Instagram
-- Used to create JS-driven dynamic web apps
-- In that regard, can be compared to Angular and Vue
+Takes components of Javascript
+Javascript representation called Virtual DOM
+Virtual DOM makes react fast
 
-What is Redux
 
-- A layer on-top of React
-- Helps with state managment of our app
+Used in REACT
+Components and Templates
 
-- data in the app
-- UI state of the app
-
-- Learn this in the 2nd Half of the playlist
-
-*/
-
-// What is there to learn..
-/*
-
-React basics
- - components, events, templates, props and forms
-
-React Router
-- routes, route parameters, redirects
-
-Redux
-- stores, actions and reducers
+- Components looks like HTML templates (actually JSX)
+- They can contain 'state' (data or UI state)
+- They also can contain JS for functionality
 
 */
