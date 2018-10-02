0939f310478d06260668215ba53705224c4a235b
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-02 21:55:31 +0000

    intro

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 // Notes
 
+// Definition of both React and Redux
 /*
 
-Notes for stuff!
+What is REACT?
+
+- Javascript library created by Facebook
+- Also used by Netflix and Instagram
+- Used to create JS-driven dynamic web apps
+- In that regard, can be compared to Angular and Vue
+
+What is Redux
+
+- A layer on-top of React
+- Helps with state managment of our app
+
+- data in the app
+- UI state of the app
+
+- Learn this in the 2nd Half of the playlist
+
+*/
+
+// What is there to learn..
+/*
+
+React basics
+ - components, events, templates, props and forms
+
+React Router
+- routes, route parameters, redirects
+
+Redux
+- stores, actions and reducers
 
 */
