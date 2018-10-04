25864d77496135d5c6f7e699dd7418b683687206
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-04 21:59:51 +0000

    State-REACT

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 // Notes
 
-// Definition of both React and Redux
 /*
 
-What is REACT?
+Component State
 
-- Javascript library created by Facebook
-- Also used by Netflix and Instagram
-- Used to create JS-driven dynamic web apps
-- In that regard, can be compared to Angular and Vue
+-JS Object
+- Describes the current state of the component 
+- data, UI-state
 
-What is Redux
-
-- A layer on-top of React
-- Helps with state managment of our app
-
-- data in the app
-- UI state of the app
-
-- Learn this in the 2nd Half of the playlist
+- The state of a component can be updated over time
+- a modal could lose
+- the data we output could change
 
 */
 
-// What is there to learn..
-/*
-
-React basics
- - components, events, templates, props and forms
-
-React Router
-- routes, route parameters, redirects
-
-Redux
-- stores, actions and reducers
-
-*/
