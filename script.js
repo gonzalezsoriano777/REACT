dd30823a4fdc32668bb19a4840b90beb0776eda7
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-08 15:32:20 +0000

    CreateReactApp

--- script.js
+++ script.js
@@ -2,8 +2,25 @@
 
 /*
 
-onMouseOver - meaning once some hovers or moves there mouse to whatever is specified then it execute the function in the console
-onCopy - meaning when you try to copy a string or integer then it will execute a line of string in the console (used index.html)
+onChange - Once you type some kind of text, whatever the object with a name that you put from the start changes to that text you submitted
 
 */
 
+// Create a React App
+/*
+
+- A command line  tool to create React apps
+- Development server 
+- Use ES6 features which are not normally supported
+- Keep our command modular
+- Use build tools to create optimized code
+
+For the React App to run the project we want to do, we use
+'npx create-react-app my-app'
+go to the project by using 'cd my-app'
+and start the package that runs inside your project 'npm start'
+
+
+ALERT! Must have at least node.js version of 5.2 or higher to actually make this work, check by using 'node -v'
+
+*/ 
