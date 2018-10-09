0a3a513ceb3e70c9e526437fc7ebf4d7bc9988a2
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-09 02:33:53 +0000

    Nesting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HG from './HG'; // This is nesting components since your importing a file and putting it into another file
 // import './App.css';
 
 class App extends Component { // Hey! Once your here it best to delete the code that's a default so that you can create your very own react app
@@ -7,6 +8,7 @@ class App extends Component { // Hey! Once your here it best to delete the code 
       <div className="App">
        <h1>My First React App</h1>
        <p>Welcome :0</p>
+       <HG />
       </div>
     );
   }
---
+++ my-app/src/HG.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HG extends Component {
+    render(){
+     return(
+         <div className="hg">
+         <div>Name: Ryu</div>
+         <div>Age: 30</div>
+         <div>Belt: Black</div>
+         </div>
+         )
+    }
+}
+
+export default HG
