9ea5066f800eac0aa24e6178f84441da8bd501e5
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-09 21:52:19 +0000

    Recap of what we did and how it's connected to the VD

--- script.js
+++ script.js
@@ -5,4 +5,7 @@
 
 Overall just a recap of what we did and how it's connected to the Virtual DOM
 
+
+Once we update our App and new virtual which is the new line of code updates the current Virtual DOM which is the Browser itself
+It's a Cycle that occuring each and every time.
 */ 
