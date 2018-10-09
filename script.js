d20dfb21c33ba28aa42e34b56cca952fdac9173e
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-09 21:48:38 +0000

    Just a Recap and How our React app works with Virtual DOM

--- script.js
+++ script.js
@@ -2,27 +2,7 @@
 
 /*
 
-onChange - Once you type some kind of text, whatever the object with a name that you put from the start changes to that text you submitted
 
-*/
-
-// Create a React App
-/*
-
-- A command line  tool to create React apps
-- Development server 
-- Use ES6 features which are not normally supported
-- Keep our command modular
-- Use build tools to create optimized code
-
-For the React App to run the project we want to do, we use
-'npx create-react-app my-app'
-go to the project by using 'cd my-app'
-and start the package that runs inside your project 'npm start'
-
-
-ALERT! Must have at least node.js version of 5.2 or higher to actually make this work, check by using 'node -v'
-
-Overal it just shows you the sources and key files that are going to be used for the application that's gonna be created
+Overall just a recap of what we did and how it's connected to the Virtual DOM
 
 */ 
