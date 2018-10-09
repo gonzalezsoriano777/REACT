f5dfd9cbeaa9042a3a2c19fd7b63f622fd531517
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-09 02:17:20 +0000

    Defining SPA and Beginning to create the REACT App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-class App extends Component {
+class App extends Component { // Hey! Once your here it best to delete the code that's a default so that you can create your very own react app
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>My First React App</h1>
+       <p>Welcome :0</p>
       </div>
     );
   }
 }
 
 export default App;
+// All of this is stuff we have done throught learning REACT and it's states
