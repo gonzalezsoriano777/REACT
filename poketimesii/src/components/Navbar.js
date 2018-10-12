e32029574d177fb941a195867cd38ef06379125c
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-12 03:17:41 +0000

    Using Axios

--- poketimesii/src/components/Navbar.js
+++ poketimesii/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link, NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
-   setTimeout(() => {
-        props.history.push('/about')
-     }, 2000) // Makes the homepage switch to the about page after 2 seconds
+   //setTimeout(() => {
+     //   props.history.push('/about')
+     //}, 2000) // Makes the homepage switch to the about page after 2 seconds
     
     return (
 <nav className="nav-wrapper red darken-3">
