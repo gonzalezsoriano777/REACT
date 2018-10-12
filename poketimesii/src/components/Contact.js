cf88d503841e8bc18e29fa3d74e960313d73ae72
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-12 00:44:55 +0000

    Programmatic-Redirect, making pages redirect to other pages

--- poketimesii/src/components/Contact.js
+++ poketimesii/src/components/Contact.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const Contact = () => {
+const Contact = (props) => { // automatically added props to the router
+//console.log(props); // if you check this in the console and go to the contact page, it makes a object(stuff that was never added, it was added by itself)
+   
+   /* setTimeout(() => {
+        props.history.push('/about');
+    }, 2000);*/ // makes the contact page switch to the about page in 2 seconds
     return (
         <div className="container">
         <h4 className="center">Contact</h4>
--- poketimesii/src/components/Navbar.js
+++ poketimesii/src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+   setTimeout(() => {
+        props.history.push('/about')
+     }, 2000) // Makes the homepage switch to the about page after 2 seconds
+    
     return (
 <nav className="nav-wrapper red darken-3">
  <div className="container">
@@ -18,4 +22,4 @@ const Navbar = () => {
  )
 }
 
-export default Navbar
+export default withRouter(Navbar) // wrapping a component, applying the properties to the prop and then supercharging a component
