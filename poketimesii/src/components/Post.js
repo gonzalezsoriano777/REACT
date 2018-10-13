e0ba88aaae614903f5c8cddfe24a9c2160e46228
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-13 14:39:16 +0000

    RouteParameters-Part2

--- poketimesii/src/components/Post.js
+++ poketimesii/src/components/Post.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 class Post extends Component {
     state = {
-        id: null
+        post: null
     }
     componentDidMount(){
         let id = this.props.match.params.post_id;
-        this.setState({
-            id: id
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+        .then(res => {
+            this.setState({
+                post: res.data
+            })
         })
     }
     render(){
+        
+    const post = this.state.post ? (
+        <div className="post"> 
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
+        </div>
+        // if we have post then it's gonna show this, with the text of the object of state
+        ) : (
+             <div className="center">Loading post..</div>
+             // if there isn't no post then it will show this..
+        )    
+        
     return (
         <div className="container">
-            <h4>{this.state.id}</h4>
+           { post } 
         </div>
     )
   }
