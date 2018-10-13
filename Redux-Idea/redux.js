19c324fb1996fb643cff6ec72225b610aec8839b
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-13 16:31:11 +0000

    Store-Subscriptions bringing in more then one object

--- Redux-Idea/redux.js
+++ Redux-Idea/redux.js
@@ -9,12 +9,25 @@ const initState = { // state is the state of the store, this passes the intial s
 function myReducer(state = initState, action){
     if (action.type == 'ADD_TODO') {
       return {
+          ...state, // grab everything in the current sate bring them into the todos and overide the todos
           todos: [...state.todos, action.todo] // speads the array of state.todos and grabs the action.todo
      }    
    }
+   
+   if (action.type == 'ADD_POST') {
+      return {
+          ...state, // grab everything in the current sate bring them into the todos and overide the todos
+          todos: [...state.posts, action.post] // speads the array of state.todos and grabs the action.todo
+     }    
+   }
 }
-const store = createStore(myReducer); 
+const store = createStore(myReducer);
 
-const todoAction = { type: 'ADD_TODO', todo: 'buy milk'}
+store.subscribe(() => {
+    console.log('state updated');
+    console.log(store.getState());
+})
 
-store.dispatch(todoAction)  // dispatch this todoAction which is gonna send it to the reducer
+store.dispatch({type: 'ADD_TODO', todo: 'buy milk'});
+store.dispatch({type: 'ADD_TODO', todo: 'sleep some more'});
+store.dispatch({type: 'ADD_POST', todo: 'Egg hunt with yoshi'});
